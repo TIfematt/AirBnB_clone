0ee2d96abeff5a9fa1b36ad3bcbbd1cfa4940ca4
Boluwatife <boluwatifeoladejo@gmail>
2022-11-22 11:26:51 +0000

    Updated basemodel test case

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Unitest for base_model
+"""
+import unittest
+from models.base_model import BaseModel
+import datetime
+
+
+class TestBaseModel(unittest.testcase):
+    """
+    Class for testing base model case
+    """
+
+    model = BaseModel()
+
+    def test_BaseModel(self):
+        """ Base model instances and attributes
+        test case
+        """
+
